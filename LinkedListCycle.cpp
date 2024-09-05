f0b32a036d6aa512866ca7def6bef7564a3a6d66
Abby McCollam <144362499+abbymccollam3@users.noreply.github.com>
2024-09-05 14:21:00 -0700

    Update LinkedListCycle.cpp

--- LinkedListCycle.cpp
+++ LinkedListCycle.cpp
@@ -10,18 +10,27 @@
 class Solution {
 public:
     bool hasCycle(ListNode* head) {
-        if (head == NULL || head->next == NULL) {
+        // this is checking that the Linked List has more
+        if (head == nullptr || head->next == nullptr) {
             return false;
         }
+
+        // creating a slow and fast pointer
         ListNode* slow = head;
-        ListNode* fast = head->next;
-        while (fast != slow) {
-            if (fast->next == NULL || fast->next->next == NULL) {
-                return false;
-            }
+        ListNode* fast = head;
+
+        // while neither pointer equals null
+        while (fast != nullptr && fast->next != nullptr) {
+            
+            // move slow and fast pointer
             slow = slow->next;
             fast = fast->next->next;
+
+            if (fast == slow) {
+                return true;
+            }
         }
-        return true;
+
+        return false;
     }
 };
