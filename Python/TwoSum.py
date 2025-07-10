b272269a0e18161a3b2cd2b951aeef8e1434aa93
Abby McCollam <144362499+abbymccollam3@users.noreply.github.com>
2025-07-09 20:45:18 -0400

    Creating 2Sum

---
+++ Python/TwoSum.py
@@ -0,0 +1,35 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        nums.sort()
+        ans = []
+        n = len(nums)
+        
+        left = 0
+        right = n - 1
+
+        while left < right:
+            
+
+        for i in range(n):
+
+
+        return ans
+        
+
+        # subtract nums[i] from target and see if we get the answer
+        # for i in range(n):
+        #     
+        #     ans.append(i)
+        #     for j in range(i+1, n):
+        #         find = target - nums[i]
+        #         print(find)
+        #         if find == 0:
+        #             ans.append(j)
+        #             return ans
+            
+        # return ans
